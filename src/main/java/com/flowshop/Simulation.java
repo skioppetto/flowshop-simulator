9a5f7b6aed93b834ee03db3287f9d44cd848f37f
Alberto Coppini <skioppetto@gmail.com>
2023-01-22 14:23:10 +0100

    testing second process step

--- src/main/java/com/flowshop/Simulation.java
+++ src/main/java/com/flowshop/Simulation.java
@@ -1,14 +1,43 @@
 package com.flowshop;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import lombok.Data;
 
-@Data public class Simulation {
+@Data
+public class Simulation {
 
    private final List<Order> orders;
+   private final Set<Workstation> workstations = new HashSet<>();
+
+   public Simulation(List<Order> orders) {
+      this.orders = orders;
+      for (Order order : orders) {
+         for (Operation operation : order.getOperations()) {
+            workstations.add(operation.getRequiredWorkstation());
+         }
+      }
+   }
 
    public void start() {
+      Operation firstOperation = orders.get(0).getOperations().get(0);
+      Workstation workstation = firstOperation.getRequiredWorkstation();
+      workstation.setCurrentOperation(firstOperation);
+      workstation.process(1);
+   }
+
+   public void process(int i) {
+      // process all workstations
+      workstations.forEach(workstation -> workstation.process(i));
+
+      for (Order order : orders) {
+         Operation op = order.getNextOperation();
+         if (null != op && null == op.getRequiredWorkstation().getCurrentOperation()) {
+            op.getRequiredWorkstation().setCurrentOperation(op);
+         }
+      }
    }
 
 }
--- src/test/java/com/flowshop/SimulatorTest.java
+++ src/test/java/com/flowshop/SimulatorTest.java
@@ -1,5 +1,8 @@
 package com.flowshop;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,26 +10,106 @@ import org.junit.jupiter.api.Test;
 
 public class SimulatorTest {
 
+   private Order buildOrder(String orderId, Workstation[] workstations, long[] cycletimes) {
+      List<Operation> operations = new ArrayList<>();
+      for (int i = 0; i < workstations.length; i++) {
+         operations.add(new Operation(orderId + " op" + (i + 1), cycletimes[i], workstations[i]));
+      }
+      return new Order(operations);
+
+   }
+
    @Test
-   void twoStationsSimulation() {
-      Workstation wst1 = new Workstation();
-      Workstation wst2 = new Workstation();
-
-      Operation op11 = new Operation("ord1 op1", 50, wst1);
-      Operation op12 = new Operation("ord1 op2", 100, wst2);
-      Order ord1 = new Order(Arrays.asList(op11, op12));
-
-      Operation op21 = new Operation("ord2 op1", 70, wst1);
-      Operation op22 = new Operation("ord2 op2", 65, wst2);
-      Order ord2 = new Order(Arrays.asList(op21, op22));
-
-      Operation op31 = new Operation("ord3 op1", 45, wst1);
-      Operation op32 = new Operation("ord3 op2", 100, wst2);
-      Order ord3 = new Order(Arrays.asList(op31, op32));
-
+   void twoStationsStartSimulation() {
+      Workstation[] workstations = new Workstation[] { new Workstation(), new Workstation() };
+      Order ord1 = buildOrder("ord1", workstations, new long[] { 50, 100 });
+      Order ord2 = buildOrder("ord2", workstations, new long[] { 70, 65 });
+      Order ord3 = buildOrder("ord3", workstations, new long[] { 45, 100 });
       Simulation sim = new Simulation(Arrays.asList(ord1, ord2, ord3));
       sim.start();
-      
+
+      // only the first workstation should be in processing
+      assertEquals(Workstation.Status.PROCESSING, workstations[0].getStatus());
+
+      // the second workstation is idle
+      assertEquals(Workstation.Status.IDLE, workstations[1].getStatus());
+
+      // ony the first operation of the first order should be in progress
+      assertEquals(Operation.Status.PROGRESS, ord1.getOperations().get(0).getStatus());
+
+      // all the other operations should be in TODO state
+      Arrays.asList(ord1.getOperations().get(1), ord2.getOperations().get(0), ord2.getOperations().get(1),
+            ord3.getOperations().get(0), ord3.getOperations().get(1))
+            .forEach(op -> assertEquals(Operation.Status.TODO, op.getStatus()));
+   }
+
+   @Test
+   void twoStationsProgress1Simulation() {
+      Workstation[] workstations = new Workstation[] { new Workstation(), new Workstation() };
+      Order ord1 = buildOrder("ord1", workstations, new long[] { 1, 2 });
+      Order ord2 = buildOrder("ord2", workstations, new long[] { 1, 1 });
+      Order ord3 = buildOrder("ord3", workstations, new long[] { 2, 1 });
+      Simulation sim = new Simulation(Arrays.asList(ord1, ord2, ord3));
+
+      sim.start(); // look at previous test for expected states
+      sim.process(1);
+
+      // first workstation should be in processing as the first operation of the first
+      // order has finished and suddenly the first op of the second order should be
+      // run
+      assertEquals(Workstation.Status.PROCESSING, workstations[0].getStatus());
+
+      // now it's time for processing the second operation of the fist order for the
+      // second workstation
+      assertEquals(Workstation.Status.PROCESSING, workstations[1].getStatus());
+      assertEquals(Operation.Status.DONE, ord1.getOperations().get(0).getStatus());
+
+      // second operation of first order should still start but now it's the current
+      // operation for the second workstation
+      assertEquals(Operation.Status.TODO, ord1.getOperations().get(1).getStatus());
+      assertEquals(ord1.getOperations().get(1), workstations[1].getCurrentOperation());
+
+      // first operation of second order should still start but now it's the current
+      // operation for the first workstation
+      assertEquals(Operation.Status.TODO, ord2.getOperations().get(0).getStatus());
+      assertEquals(ord2.getOperations().get(0), workstations[0].getCurrentOperation());
+
+      Arrays.asList(ord2.getOperations().get(1),
+            ord3.getOperations().get(0), ord3.getOperations().get(1))
+            .forEach(op -> assertEquals(Operation.Status.TODO, op.getStatus()));
+   }
+
+   @Test
+   void twoStationsProgress2Simulation() {
+      Workstation[] workstations = new Workstation[] { new Workstation(), new Workstation() };
+      Order ord1 = buildOrder("ord1", workstations, new long[] { 1, 2 });
+      Order ord2 = buildOrder("ord2", workstations, new long[] { 1, 1 });
+      Order ord3 = buildOrder("ord3", workstations, new long[] { 2, 1 });
+      Simulation sim = new Simulation(Arrays.asList(ord1, ord2, ord3));
+
+      sim.start();
+      sim.process(1); // look at previous test for expected states
+      sim.process(1);
+
+      // first operation of second order should be done on the first workstation
+      assertEquals(Operation.Status.DONE, ord2.getOperations().get(0).getStatus());
+      // first operation of third order should be ready on first workstation
+      assertEquals(ord3.getOperations().get(0), workstations[0].getCurrentOperation());
+      assertEquals(Operation.Status.TODO, ord3.getOperations().get(0).getStatus());
+
+      // second operation of first order should still be in processin on the second
+      // workstation
+      assertEquals(Operation.Status.PROGRESS, ord1.getOperations().get(1).getStatus());
+      assertEquals(ord1.getOperations().get(1), workstations[1].getCurrentOperation());
+
+      // as second workstation is still busy as it's processing the second operation
+      // of the first order,
+      // second operation of the second order should wait as its workstation is busy
+      assertEquals(Operation.Status.TODO, ord2.getOperations().get(1).getStatus());
+
+      Arrays.asList(ord3.getOperations().get(0), ord3.getOperations().get(1))
+            .forEach(op -> assertEquals(Operation.Status.TODO, op.getStatus()));
+
    }
 
 }
