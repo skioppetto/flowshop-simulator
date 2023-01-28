8a030e50e2a172a8d501fbceef878f42a9bf3a33
Alberto Coppini <skioppetto@gmail.com>
2023-01-28 22:26:28 +0100

    fixed ConcurrentModificationException

--- src/main/java/com/flowshop/ObservableSimObject.java
+++ src/main/java/com/flowshop/ObservableSimObject.java
@@ -1,23 +1,37 @@
 package com.flowshop;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public abstract class ObservableSimObject {
 
    private final List<SimObjectObserver> observers = new ArrayList<SimObjectObserver>();
+   private boolean runningNotification = false;
+   private final Set<SimObjectObserver> toBeRemoved = new HashSet<>();
 
    public void addSimObjectObserver(SimObjectObserver observer) {
       this.observers.add(observer);
    }
 
    public void notifySimObjectObservers() {
-      for (SimObjectObserver observer : observers)
-         observer.onChange(this);
+      runningNotification = true;
+      Iterator<SimObjectObserver> observerIt = observers.iterator();
+      while (observerIt.hasNext())
+         observerIt.next().onChange(this);
+      runningNotification = false;
+      observers.removeAll(toBeRemoved);
+      toBeRemoved.clear();
+
    }
 
    public void removeSimObjectObserver(SimObjectObserver observer) {
-      this.observers.remove(observer);
+      if (runningNotification)
+         toBeRemoved.add(observer);
+      else
+         this.observers.remove(observer);
    }
 
 }
