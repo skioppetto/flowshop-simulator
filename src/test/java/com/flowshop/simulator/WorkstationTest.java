ef7f5aa176643e819543caee81358a7666a6c3c8
Alberto Coppini <skioppetto@gmail.com>
2023-02-05 00:21:40 +0100

    removed useless imports

--- src/test/java/com/flowshop/simulator/WorkstationTest.java
+++ src/test/java/com/flowshop/simulator/WorkstationTest.java
@@ -6,9 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
